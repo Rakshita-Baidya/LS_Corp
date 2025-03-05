6a23b07bb480085d58f8a29c88b9449c7e56bd1b
Rakshita Baidya <119068178+Rakshita-Baidya@users.noreply.github.com>
2025-03-05 08:01:32 +0545

    updated navigationbar

--- 22067662_LS_Corp/MasterPage.Master.cs
+++ 22067662_LS_Corp/MasterPage.Master.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web.UI.WebControls;
 
 namespace _22067662_LS_Corp
 {
@@ -7,21 +6,6 @@ namespace _22067662_LS_Corp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                string currentPage = System.IO.Path.GetFileName(Request.Path).ToLower();
-
-                foreach (MenuItem item in Menu1.Items)
-                {
-                    string navigateUrl = item.NavigateUrl.ToLower().Replace("~", "").TrimStart('/');
-
-                    if (navigateUrl == currentPage)
-                    {
-                        item.Selected = true;
-                        break;
-                    }
-                }
-            }
         }
     }
 }
